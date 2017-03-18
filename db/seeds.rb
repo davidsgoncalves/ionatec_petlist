022770069d7aeddbc70afbfebc95c991b8fe703e
David <davidsg.dev@gmail.com>
2017-03-18 17:17:50 -0300

    adding breeds to BD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Breed.count == 0
+  puts 'Creating breeds!'
+  File.open('db/breeds.txt', 'r').each_line do |line|
+    Breed.create! name: line.strip
+  end
+end
