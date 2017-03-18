910897fc7d141fe4918225e15407e0f15b6e94f9
David <davidsg.dev@gmail.com>
2017-03-18 16:37:09 -0300

    fix migrations

--- db/migrate/20170318191436_create_breeds.rb
+++ db/migrate/20170318191436_create_breeds.rb
@@ -1,10 +1,8 @@
 class CreateBreeds < ActiveRecord::Migration[5.0]
   def change
     create_table :breeds do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: true
       t.timestamps
     end
-
-    add_index :name
   end
 end
--- db/migrate/20170318191650_create_dogs.rb
+++ db/migrate/20170318191650_create_dogs.rb
@@ -1,13 +1,13 @@
 class CreateDogs < ActiveRecord::Migration[5.0]
   def change
     create_table :dogs do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: true
       t.integer :gender_cd, null: false
       t.references :breeds, null: false
       t.integer :castrate_cd, null: false
       t.date :birth_date, null: false
-      t.string :owner_name, null: false
-      t.string :owner_phone, null: false
+      t.string :owner_name, null: false, index: true
+      t.string :owner_phone, null: false, index: true
       t.date :last_visit, null: false
     end
   end
